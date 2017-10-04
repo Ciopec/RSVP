94208a5c0661b45a16010d384d304a7324c1d82b
LucianC <lucian.ciopec@gmail.com>
2017-10-04 22:13:36 +0300

    Prevented default event form

--- js/app.js
+++ js/app.js
@@ -2,5 +2,6 @@ const form = document.getElementById('registrar');
 const input = form.querySelector('input');
 
 form.addEventListener('submit',(e) => {
+    e.preventDefault();
     console.log(input.value);
 });
