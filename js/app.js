085ce33451149f99bb13e93e27e64ce803bc4afc
LucianC <lucian.ciopec@gmail.com>
2017-10-04 22:11:07 +0300

    Tested app.js file.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-const form = document.getElementById
+const form = document.getElementById('registrar');
+const input = form.querySelector('input');
+
+form.addEventListener('submit',(e) => {
+    console.log(input.value);
+});
