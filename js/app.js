3c8b37f6862dc0e96e69331dc87b7f8b6943c22b
LucianC <lucian.ciopec@gmail.com>
2017-10-05 20:58:24 +0300

    added remove button code clean

--- js/app.js
+++ js/app.js
@@ -2,18 +2,29 @@ const form = document.getElementById('registrar');
 const input = form.querySelector('input');
 const ul = document.getElementById('invitedList');
 
-form.addEventListener('submit',(e) => {
-    e.preventDefault();
-    const text = input.value;
-    input.value = '';
+function createLi(text) {
     const li = document.createElement('li');
     li.textContent = text;
+
     const label = document.createElement('label');
     label.textContent = 'Confirmed';
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     label.appendChild(checkbox);
     li.appendChild(label);
+
+    const button = document.createElement('button');
+    button.textContent = 'remove';
+    li.appendChild(button);
+    
+    return li;
+}
+
+form.addEventListener('submit',(e) => {
+    e.preventDefault();
+    const text = input.value;
+    input.value = '';
+    const li = createLi(text);
     ul.appendChild(li);
 });
 
@@ -24,4 +35,9 @@ ul.addEventListener('change', (e) => {
         listItem.className = 'responded';
         else listItem.className = '';
 
+});
+
+ul.addEventListener('click', (e) => {
+    if(e.target.tagName === 'BUTTON')
+    e.target.parentNode.remove();
 });
