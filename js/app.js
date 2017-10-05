bba49331660e86ece9bfb732d0cdba12ec14e0d6
LucianC <lucian.ciopec@gmail.com>
2017-10-05 20:22:54 +0300

    Changed bg Add invitees feature Add Confirmation feature

--- js/app.js
+++ js/app.js
@@ -1,13 +1,23 @@
 const form = document.getElementById('registrar');
 const input = form.querySelector('input');
+const ul = document.getElementById('invitedList');
 
 form.addEventListener('submit',(e) => {
     e.preventDefault();
     const text = input.value;
     input.value = '';
-    const ul = document.getElementById('invitedList');
     const li = document.createElement('li');
     li.textContent = text;
+    const label = document.createElement('label');
+    label.textContent = 'Confirmed';
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    label.appendChild(checkbox);
+    li.appendChild(label);
     ul.appendChild(li);
+});
+
+ul.addEventListener('change', (e) => {
+    console.log(e.target.checked);
 
 });
